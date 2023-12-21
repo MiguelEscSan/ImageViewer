a59aa2e18f90ae80b011d6ccfed2dc2c60feff00
Trepuco03 <106118289+Trepuco03@users.noreply.github.com>
2023-12-21 13:50:15 +0000

    Image Viewer, all done, but cannot show 3 images at the same time

--- src/main/java/software/ulpgc/ImageViewer/model/Picture.java
+++ src/main/java/software/ulpgc/ImageViewer/model/Picture.java
@@ -2,5 +2,5 @@ package software.ulpgc.ImageViewer.model;
 
 import software.ulpgc.ImageViewer.interfaces.Image;
 
-public record Paint(Image image, int offset) {
+public record Picture(Image image, int offset) {
 }
